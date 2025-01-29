396eaf53ffbcea31b84654d1d99efb9c1a02af93
jannatunn <jannatunn4500@gmail.com>
2025-01-29 18:37:27 +0700

    layout routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,12 @@
 import { index, layout, route, type RouteConfig } from "@react-router/dev/routes";
 
+
+// export default [
+//   index("routes/home.tsx"),
+//   route("contacts/:contactId", "routes/contact.tsx"),
+//   route("about", "routes/about.tsx"),
+// ] satisfies RouteConfig;
+
 export default [
   layout("layouts/sidebar.tsx", [
     index("routes/home.tsx"),
